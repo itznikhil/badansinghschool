a2592006f2ac030fb8ada9fae90c97cc67eaefbe
nikhil garg <nikhilgargcoder@gmail.com>
2022-06-19 16:58:54 +0530

    UPdated code

--- src/Pages/exams/Exams.jsx
+++ src/Pages/exams/Exams.jsx
@@ -1,27 +1,56 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import baseUrl from "../../baseUrl";
+import axios from "axios";
+
 export const Exams = () => {
   const [inputVal, setInputVal] = useState({
-    year: '',
-    class: '',
-  })
+    year: "",
+    class: "",
+  });
 
   const handleInput = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setInputVal({
       ...inputVal,
       [name]: value,
-    })
-  }
+    });
+  };
+
+  const handleSubmit = () => {
+    var formData = {
+      year: inputVal.year,
+      class: inputVal.class,
+    };
+    axios
+      .post(baseUrl + "/api/v1/result/getByClassAndYear", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        if (res.data.length === 0) {
+          window.alert("No results found");
+        } else {
+          window.location.href =
+            baseUrl + "/uploads/" + res.data[0].file.split("\\")[1];
+          window.alert("Successfully Downloaded");
+          setInputVal("");
+        }
+      })
+      .catch((err) => {
+        window.alert(err.response.data.message);
+      });
+  };
 
   return (
-    <div className='pb-5'>
-      <div className='container mt-5'>
-        <h3 className='text-center fw-bold orange-text-color'>
+    <div className="pb-5">
+      <div className="container mt-5">
+        <h3 className="text-center fw-bold orange-text-color">
           Exams Procedures
         </h3>
-        <div className='mt-5 col-12'>
-          <p className='fw-bold'>For Class I to V:</p>
+        <div className="mt-5 col-12">
+          <p className="fw-bold">For Class I to V:</p>
           <ul>
             <li>First Quarterly Exams: From 25th July to 30th July.</li>
             <li>Second Quarterly Exams: From 25th October to 30th October.</li>
@@ -32,8 +61,8 @@ export const Exams = () => {
             <li>Result Declaration: On 31st March.</li>
           </ul>
         </div>
-        <div className='mt-5 col-12'>
-          <p className='fw-bold'>For Class VI to VIII:</p>
+        <div className="mt-5 col-12">
+          <p className="fw-bold">For Class VI to VIII:</p>
           <ul>
             <li>First Monthly Exams: From 15th July to 20th July.</li>
             <li>First Quarterly Exams: From 10th October to 20th October.</li>
@@ -44,56 +73,60 @@ export const Exams = () => {
             <li>Annual Exams: From 10th March to 25th March.</li>
             <li>Result Declaration: On 31st March.</li>
           </ul>
-          <hr className='mt-5 orange-bg-color'/>
+          <hr className="mt-5 orange-bg-color" />
         </div>
-        <h3 className='text-center fw-bold orange-text-color mt-5'>
+        <h3 className="text-center fw-bold orange-text-color mt-5">
           Download Result
         </h3>
-        <div className='col-lg-6 col-md-12 col-sm-12 mt-5'>
-          <div className='mb-3 row'>
-            <label htmlFor='input1' className='col-sm-2 col-form-label fw-bold'>
+        <div className="col-lg-6 col-md-12 col-sm-12 mt-5">
+          <div className="mb-3 row">
+            <label htmlFor="input1" className="col-sm-2 col-form-label fw-bold">
               Year
             </label>
-            <div className='col-sm-10'>
+            <div className="col-sm-10">
               <input
-                type='text'
-                className='form-control'
-                id='input1'
-                placeholder='Enter year ...'
-                name='year'
+                type="text"
+                className="form-control"
+                id="input1"
+                placeholder="Enter year ..."
+                name="year"
                 value={inputVal.year}
                 onChange={handleInput}
               />
             </div>
           </div>
-          <div className='mb-3 row'>
-            <label htmlFor='input2' className='col-sm-2 col-form-label fw-bold'>
+          <div className="mb-3 row">
+            <label htmlFor="input2" className="col-sm-2 col-form-label fw-bold">
               Class
             </label>
-            <div className='col-sm-10'>
+            <div className="col-sm-10">
               <input
-                type='text'
-                className='form-control'
-                id='input2'
-                placeholder='Enter class ...'
-                name='class'
+                type="text"
+                className="form-control"
+                id="input2"
+                placeholder="Enter class ..."
+                name="class"
                 value={inputVal.class}
                 onChange={handleInput}
               />
             </div>
           </div>
         </div>
-        <div className='col-lg-12 col-sm-12 col-md-12'>
-          <div className='row'>
-            <div className='col-sm-10'>
-              <button type='button' className='btn orange-bg-color text-white'>
+        <div className="col-lg-12 col-sm-12 col-md-12">
+          <div className="row">
+            <div className="col-sm-10">
+              <button
+                type="button"
+                className="btn orange-bg-color text-white"
+                onClick={handleSubmit}
+              >
                 Download
               </button>
               &nbsp;&nbsp;
               <Link
-                to='/'
-                type='button'
-                className='btn orange-bg-color text-white'
+                to="/"
+                type="button"
+                className="btn orange-bg-color text-white"
               >
                 Go to Home
               </Link>
@@ -102,5 +135,5 @@ export const Exams = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
